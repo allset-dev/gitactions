b007738d24a6e55fbfae550b0779d34a46c0003d
subbiah chandramouli <58955584+subbiah-logichub@users.noreply.github.com>
2022-04-04 22:52:33 -0500

    asd

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const github = require('@actions/github');
 
 try {
     const token = core.getInput('token', { required: true });
-    const octokit = github.getOctokit(token);
     const githubInfo = github?.context?.payload;
-    const { ref = '', pull_request = {} } = githubInfo;
+    const { ref = '', pull_request } = githubInfo;
+
+    if (pull_request === null) {
+        core.setFailed("No pull request found");
+        return;
+    }
+
+    if (!token) {
+        core.setFailed("TOKEN input is required");
+        return;
+    }
 
     const branchName = ref?.replace?.('refs/heads/', '') || '';
     const [repoOwner, repoName] = process?.env?.GITHUB_REPOSITORY?.split?.('/') || [];
@@ -17,9 +26,8 @@ try {
     console.log(`The event payload: ${JSON.stringify(githubInfo, undefined, 2)}`);
 
 
+    const octokit = github.GitHub(token);
     octokit.pulls.update({
-        owner: repoOwner,
-        repo: repoName,
         body: `https://logichub.atlassian.net/browse/${branchName}`,
         pull_number: pull_request.number,
     });
