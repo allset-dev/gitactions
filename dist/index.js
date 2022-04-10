c65b40845b8263261b6cff051de99b91a0cb0b0e
subbiah chandramouli <58955584+subbiah-logichub@users.noreply.github.com>
2022-04-10 15:28:03 -0500

    Multiple git actions in same flow

--- dist/index.js
+++ dist/index.js
@@ -31,29 +31,42 @@ async function updatePrDesc() {
     const token = core.getInput('GITHUB_TOKEN', { required: true });
 
     const githubInfo = github?.context?.payload || {};
-    const { base, pull_request } = githubInfo;
-    const branchName = base?.ref || '';
-
-    const jiraId = /allset-\d+/.exec(branchName)?.[0] || '';
+    const { base, head, pull_request } = githubInfo;
     const [repoOwner = '', repoName = ''] = process?.env?.GITHUB_REPOSITORY?.split?.('/') || [];
-    const jiraLink = jiraId? `https://logichub.atlassian.net/browse/${jiraId}`: '';
     const pull_number = pull_request?.number || null;
+    const baseBranchName = base?.ref || '';
+    const headBranchName = head?.ref || '';
+    const bodyArray = [];
 
-    console.log(`pull_request: ${pull_request}`)
-    console.log(`jiraLink: ${jiraLink}`);
+    const jiraEpicLink = getJiraLinkFromString({string: headBranchName});
+
+    if(jiraEpicLink) {
+        bodyArray.push('Jira epic link:');
+        bodyArray.push(`\t${jiraEpicLink}`);
+    }
+
+    const jiraBugLink = getJiraLinkFromString({string: baseBranchName});
+
+    if(jiraBugLink) {
+        bodyArray.push('Jira story/bug link:');
+        bodyArray.push(`\t${jiraBugLink}`);
+    }
+
+    const body = bodyArray.join('\n');
+
+    console.log(`jiraId: ${baseBranchName}, ${headBranchName}, ${body}`)
     console.log(`pull_number: ${pull_number}`);
     console.log(`repo: ${repoOwner}, ${repoName}`);
     console.log(`The event payload: ${JSON.stringify(githubInfo, undefined, 2)}`);
 
 
-
-    if(jiraId && repoOwner && repoName && pull_number){
+    if(body && repoOwner && repoName && pull_number){
         const octokit = github.getOctokit(token);
         await octokit.request(`PATCH /repos/${repoOwner}/${repoName}/pulls/${pull_number}`, {
             owner: repoOwner,
             repo: repoName,
             pull_number,
-            body: jiraLink
+            body,
         });
     }else{
         if(pull_number){
@@ -62,6 +75,12 @@ async function updatePrDesc() {
             core.setFailed("Update-pr-desc: some requested parameters are empty, check above console logs.");
         }
     }
+}
+
+function getJiraLinkFromString({string}) {
+    const jiraId = /allset-\d+/.exec(string)?.[0] || '';
+
+    return jiraId ? `https://logichub.atlassian.net/browse/${jiraId}`: '';
 }
 
 /***/ }),
